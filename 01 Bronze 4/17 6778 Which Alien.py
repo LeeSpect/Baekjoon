fef5046cd9a96ae123926305cb77597b18c4f3d4
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-07 12:55:07 +0900

    Create 17 6778 Which Alien.py

---
+++ 01 Bronze 4/17 6778 Which Alien.py
@@ -0,0 +1,10 @@
+# 17 6778 Which Alien?.py
+
+a = int(input()); b = int(input())
+
+if a >= 3 and b <= 4:
+    print('TroyMartian')
+if a <= 6 and b >= 2:
+    print('VladSaturnian')
+if a <= 2 and b <= 3:
+    print('GraemeMercurian')
