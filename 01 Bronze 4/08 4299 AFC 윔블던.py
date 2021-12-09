b3e295f90b691632724112102554fe64a8a1c37c
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-10 01:27:34 +0900

    Update 08 4299 AFC 윔블던.py

--- 01 Bronze 4/08 4299 AFC 윔블던.py
+++ 01 Bronze 4/08 4299 AFC 윔블던.py
@@ -1,9 +1,10 @@
 # 08 4299 AFC 윔블던.py
 
-a,b = map(int, input().split())
-print((a+b)//2, a-(a+b)//2)
+x,y = map(int, input().split())
+a = (x+y)/2; b = x-a
+if (x+y)%2 == 0 and x>=y:
+    print(int(a), int(b))
+else:
+    print(-1)
 
-# 5:3 8 2
-# 7:2 9 5
-# 10:8 18 2
-# 100:53 153 47
+# 2A = x+y
