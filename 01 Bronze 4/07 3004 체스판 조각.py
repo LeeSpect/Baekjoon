a1993b2336b2519c644266521461fea8392d6ab5
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-10 01:02:32 +0900

    Update 07 3004 체스판 조각.py

--- 01 Bronze 4/07 3004 체스판 조각.py
+++ 01 Bronze 4/07 3004 체스판 조각.py
@@ -1,4 +1,19 @@
 # 07 3004 체스판 조각.py
 
-A = int(input())
-print(A*2)
+def odd(n):
+    if n == 1:
+        return 2
+    else:
+        return odd(n-2) + n + 1
+
+def even(n):
+    if n == 2:
+        return 4
+    else:
+        return even(n-2) + n + 1
+
+n = int(input())
+if n%2 == 1:
+    print(odd(n))
+else:
+    print(even(n))
