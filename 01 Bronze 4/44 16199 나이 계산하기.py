900a6fb87fe84d1f2b682e36c66474445183c986
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-12 19:50:44 +0900

    Create 44 16199 나이 계산하기.py

---
+++ 01 Bronze 4/44 16199 나이 계산하기.py
@@ -0,0 +1,16 @@
+# 44 16199 나이 계산하기.py
+
+l = list(map(int, input().split()))
+l2 = list(map(int, input().split()))
+
+#만나이
+if l2[1] > l[1] or (l2[1]==l2[1] and l2[2]>l[2]):
+    print(l2[0]-l[0])
+else:
+    print(l2[0]-l[0]-1)
+
+#세는 나이
+print(l2[0]-l[0]+1)
+
+#연 나이
+print(l2[0]-l[0])
