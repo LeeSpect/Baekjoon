3479b56bbb2dd2fc369a7a2517bc36a647410c3d
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-22 16:58:35 +0900

    Create 010 4623 Copier Reduction.py

---
+++ 01 Bronze 3/010 4623 Copier Reduction.py
@@ -0,0 +1,21 @@
+# 010 4623 Copier Reduction
+
+while True:
+    a,b,c,d = map(int, input().split())
+    if a==b==c==d==0: break
+    else:
+        if bool(a-b >= 0) == bool(c-d >= 0):
+            if a<=c and b<=d:
+                print('100%')
+            else:
+                if int(c/a*100) < int(d/b*100):
+                    print(f'{int(c/a*100)}%')
+                else: print(f'{int(d/b*100)}%')
+        else:
+            if a<=d and b<=c:
+                print('100%')
+            else:
+                if int(d/a*100) < int(c/b*100):
+                    print(f'{int(d/a*100)}%')
+                else: print(f'{int(c/b*100)}%')
+            
