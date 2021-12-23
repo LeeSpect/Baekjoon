4182af5e15e75e910ab14cf953de7daa273efc25
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-23 16:40:49 +0900

    Create 014 5101 Sequences.py

---
+++ 01 Bronze 3/014 5101 Sequences.py
@@ -0,0 +1,10 @@
+# 014 5101 Sequences.py
+
+while True:
+    a,b,c = map(int, input().split())
+    if a==b==c==0: break
+    else:
+        if (c-a)%b == 0:
+            print((c-a)//b+1)
+        else:
+            print('X')
