2b2c1730c98744db99e1d745b596db8e1f7d7ec5
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-23 16:10:10 +0900

    Create 011 5073 삼각형과 세 변.py

---
+++ 01 Bronze 3/011 5073 삼각형과 세 변.py
@@ -0,0 +1,16 @@
+# 011 5073 삼각형과 세 변.py
+
+while True:
+    l = list(map(int, input().split()))
+    if sum(l) == 0: break
+    else:
+        l.sort()
+        if l[2] >= l[0] + l[1]:
+            print('Invalid')
+        elif l[0]==l[1]==l[2]:
+            print('Equilateral')
+        elif l[0]==l[1] or l[0]==l[2] or l[1]==l[2]:
+            print('Isosceles')
+        else:
+            print('Scalene')
+    
