15ab34e2f060b11958d0be96d3c4b87d7a5dee70
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2021-12-26 16:10:44 +0900

    Update 026 5618 공약수.py

--- 01 Bronze 3/026 5618 공약수.py
+++ 01 Bronze 3/026 5618 공약수.py
@@ -1,4 +1,4 @@
-# 026 5618 공약수.py
+# 026 5618 공약수.py (틀린 코드)
 
 import sys
 
@@ -12,3 +12,19 @@ for i in range(1, l[n-1]):
     else:
         if l[0]%i == 0 and l[1]%i == 0 and l[2]%i == 0:
             print(i)
+            
+# 026 5618 공약수.py (수정 코드)
+# 수정 코드 : gcd 함수 사용
+
+from math import gcd
+
+
+n = int(input())
+l = list(map(int, input().split()))
+l.sort()
+g = gcd(l[0], l[1])
+for i in range(1, n):
+    new_g = gcd(g, l[i])    
+for _ in range(1,new_g+1):
+    if new_g%_ == 0:
+        print(_)
