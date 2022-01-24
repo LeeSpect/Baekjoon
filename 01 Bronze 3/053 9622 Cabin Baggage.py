65c8d6b4df9299fd1cbc00a9027a955aa18df744
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2022-01-24 10:36:57 +0900

    Update 053 9622 Cabin Baggage.py

--- 01 Bronze 3/053 9622 Cabin Baggage.py
+++ 01 Bronze 3/053 9622 Cabin Baggage.py
@@ -1,5 +1,6 @@
 # 053 9622 Cabin Baggage.py
 
+# 틀린 
 import sys
 
 t = int(sys.stdin.readline())
@@ -12,3 +13,19 @@ for _ in range(t):
         print(1)
         cnt += 1
 print(cnt)        
+
+
+# 수정 코드 : ((l > 56 or wi > 45 or d > 25) and l+wi+d > 125) or we > 7 : if 문의 조건을 잘 확인하지 못했음.
+
+import sys
+
+t = int(sys.stdin.readline())
+cnt = 0
+for _ in range(t):
+    l,wi,d,we = map(float, sys.stdin.readline().split())
+    if ((l > 56 or wi > 45 or d > 25) and l+wi+d > 125) or we > 7:
+        print(0)
+    else:
+        print(1)
+        cnt += 1
+print(cnt)       
