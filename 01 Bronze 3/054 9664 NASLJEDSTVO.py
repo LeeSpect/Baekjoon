fdef14f37b2e22cb3ece0de05915c4646285d9b6
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2022-01-24 10:57:24 +0900

    Update 054 9664 NASLJEDSTVO.py

--- 01 Bronze 3/054 9664 NASLJEDSTVO.py
+++ 01 Bronze 3/054 9664 NASLJEDSTVO.py
@@ -1,5 +1,5 @@
 # 054 9664 NASLJEDSTVO.py
-
+# 틀린 
 import sys
 
 a = int(sys.stdin.readline())
@@ -9,3 +9,15 @@ if (b*a/(a-1))%2 == 0:
     print(int(b*a/(a-1))-1, int(b*a/(a-1)))
 else:
     print(int(b*a/(a-1)), int(b*a/(a-1)))
+
+    
+# 수정 코드
+import sys
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+
+if b/(a-1)%1==0:
+    print(b//(a-1)-1+b, b//(a-1)+b)
+else:
+    print(b//(a-1)+b, b//(a-1)+b)
