a1e3814b1572ffe4cee2544fc721d69065669e10
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2022-06-17 09:49:47 +0900

    Create 2566 최댓값.py

---
+++ 01 Bronze 3/2566 최댓값.py
@@ -0,0 +1,11 @@
+import sys; input=sys.stdin.readline
+
+m = -1; idx = 0; row = 0
+for i in range(9):
+    l1 = list(map(int, input().split()))
+    if max(l1) > m:
+        m = max(l1)
+        idx = l1.index(max(l1))+1
+        row = i+1
+print(m)
+print(row, idx)
