7250c52dff1f039384f780189207d5e1d3fdb43b
JuHyung Lee <64893709+LeeSpect@users.noreply.github.com>
2023-05-11 09:24:21 +0900

    Update 2566 최댓값.py

--- Bronze 3/2566 최댓값.py
+++ Bronze 3/2566 최댓값.py
@@ -1,9 +1,12 @@
-m = 0; idx = 0; row = 0
+import sys
+input = sys.stdin.readline
+
+maxi, r, c = 0, 0, 0
 for i in range(9):
-    l1 = list(map(int, input().split()))
-    if max(l1) > m:
-        m = max(l1)
-        idx = l1.index(max(l1))+1
-        row = i+1
-print(m)
-print(row, idx)
+    arr = list(map(int, input().split()))
+    for j in range(9):
+        if arr[j] > maxi:
+            maxi = arr[j]
+            r, c = i, j
+print(maxi)
+print(r + 1, c + 1)
